1fe113afc337ccf437c4280730ecb9095c7ba786
AndressaSales <andressaccs2001@gmail.com>
2024-06-04 17:55:01 -0300

    acai

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <div>
-            <NuxtImg src="" />
+        <div class="flex-cols-1">
+            <div class="justify-center items-center flex lg:float-right">
+                <NuxtImg class="lg:w-max w-70" src="../public/image/banner2.png" />
+            </div>
+            <h1 class="text-2xl lg:text-5xl pt-5 lg:pt-60 lg:pb-6 lg:pl-5 lg:z-40 font-bold ms:text-center text-orange-500">Experimente o Melhor Açaí</h1>
+            <p class="text-sm font-serif pt-2 ms:text-center lg:pl-5">Faça seu pedido hoje mesmo e sinta a diferença do açaí de alta qualidade. Temos a opção perfeita para você. Navegue pela nossa loja e descubra o verdadeiro sabor!</p>
+            <div class="mt-3 pb-4 flex gap-3 lg:pt-5 lg:pl-4 ms:justify-center ms:flex ms:items-center">
+                <NuxtLink class="text-white bg-orange-500 rounded-md text-center h-7 w-24 hover:bg-transparent hover:text-orange-400 hover:border-2 hover:rounded-none hover:border-b-purple-950" to="/menu">Menu</NuxtLink>
+            </div>
         </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="">
+    <div>
         <Banner/>
     </div>
 </template>
